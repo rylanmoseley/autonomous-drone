2e23093b941e417f1e21f8c8097725f2ef27d3cd
Rylan Moseley <rylanmoseley@gmail.com>
2026-09-01 11:06:55 -0400

    test(sim): update mock hardware with consolidated motor interface

--- flight-control/desktop_sim/mock_hardware.cpp
+++ flight-control/desktop_sim/mock_hardware.cpp
@@ -17,10 +17,26 @@ void MockMotors::init() {
     std::cout << "[MockMotors] Initialized." << std::endl;
 }
 
-void MockMotors::set_torque(int motor_index, units::torque::newton_meter_t output) {
+void MockMotors::set_torque(int motor_id, units::torque::newton_meter_t output) {
     // Intentionally empty to avoid spamming the console at 100Hz
 }
 
-units::angular_velocity::revolutions_per_minute_t MockMotors::get_rpm(int motor_index) {
+void MockMotors::set_speed(int motor_id, units::angular_velocity::revolutions_per_minute_t speed) {
+    // Intentionally empty
+}
+
+units::angular_velocity::revolutions_per_minute_t MockMotors::get_speed(int motor_id) {
     return units::angular_velocity::revolutions_per_minute_t(0.0);
+}
+
+units::current::amperes_t MockMotors::get_current(int motor_id) {
+    return units::current::amperes_t(0.0);
+}
+
+units::temperature::celsius_t MockMotors::get_temperature(int motor_id) {
+    return units::temperature::celsius_t(25.0);
+}
+
+units::time::millisecond_t MockMotors::get_time_since_last_message(int motor_id) {
+    return units::time::millisecond_t(0.0);
 }
--- flight-control/desktop_sim/mock_hardware.h
+++ flight-control/desktop_sim/mock_hardware.h
@@ -12,6 +12,12 @@ public:
 class MockMotors : public HAL::MotorInterface {
 public:
     void init() override;
-    void set_torque(int motor_index, units::torque::newton_meter_t output) override;
-    units::angular_velocity::revolutions_per_minute_t get_rpm(int motor_index) override;
+    void set_torque(int motor_id, units::torque::newton_meter_t output) override;
+    void set_speed(int motor_id, units::angular_velocity::revolutions_per_minute_t speed) override;
+
+    units::angular_velocity::revolutions_per_minute_t get_speed(int motor_id) override;
+    units::current::amperes_t get_current(int motor_id) override;
+    units::temperature::celsius_t get_temperature(int motor_id) override;
+
+    units::time::millisecond_t get_time_since_last_message(int motor_id) override;
 };
